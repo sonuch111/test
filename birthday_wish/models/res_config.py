cbd0ed6743df845bb39477e8525367091dbdef63
Sonu Chaudhary <s.chaudhary@econostic.net>
2018-09-29 08:22:33 +0530

    backend_theme_v11/

---
+++ birthday_wish/models/res_config.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    @api.model
+    def _get_default_birthday_mail_template(self):
+        return self.env.user.company_id.birthday_mail_template and self.env.user.company_id.birthday_mail_template.id or False
+
+    birthday_mail_template = fields.Many2one('mail.template', 'Birthday Wishes Template',
+        default=_get_default_birthday_mail_template, required=True,
+        help='This will set the default mail template for birthday wishes.')
+
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            birthday_mail_template=self.env['ir.config_parameter'].sudo().get_param('birthday_mail_template')
+        )
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('birthday_mail_template', self.birthday_mail_template.id)
+
+    @api.multi
+    def set_birthday_mail_template(self):
+        if self.birthday_mail_template:
+            self.env.user.company_id.write({'birthday_mail_template': self.birthday_mail_template.id})
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+ 
---
+++ birthday_wish/models/res_partner.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+from odoo import api, fields, models, _
+from datetime import datetime, timedelta, time
+
+
+class res_partner(models.Model):
+    _inherit = 'res.partner'
+
+    birth_date = fields.Date('Date Of Birth')
+
+    @api.multi
+    def send_birthday_email(self):
+        ir_data = self.env['ir.model.data']
+        wish_template = ir_data.get_object('birthday_wish', 'email_template_birthday_wish')
+        channel = ir_data.get_object('birthday_wish', 'channel_birthday')
+        today = datetime.now()
+        today_month_day = '%-' + today.strftime('%m') + '-' + today.strftime('%d')
+        for partner in self.search([('birth_date', 'like', today_month_day), ('is_company', '=', False)]):
+            if partner.email:
+                if partner.company_id.birthday_mail_template and partner.company_id.birthday_mail_template.id:
+                    partner.company_id.birthday_mail_template.sudo().send_mail(partner.id, force_send=True)
+                else:
+                    wish_template.sudo().send_mail(partner.id, force_send=True)
+            # res = channel.message_post(body=_('Happy Birthday Dear %s.') % (partner.name), partner_ids=[partner.id])
+            # res.write({'channel_ids':[[6, False, [channel.id]]]})
+            # partner.message_post(body=_('Happy Birthday.'))
+        return None
